47bd3fc287bc83129e31ead7fa667250c6b373b1
David Petrofsky <davidpet@ucla.edu>
2015-12-12 00:53:55 -0800

    Adding new Cocoa test application.

---
+++ OSX/Hello World/Hello World/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by David Petrofsky on 12/12/15.
+//  Copyright © 2015 David Petrofsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ OSX/Hello World/Hello World/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  Hello World
+//
+//  Created by David Petrofsky on 12/12/15.
+//  Copyright © 2015 David Petrofsky. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+    NSLog(@"This is a hello world application");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
