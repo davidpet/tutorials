058b7e8fa13aec96e733242184f1e46639a57b7f
David Petrofsky <davidpet@ucla.edu>
2016-09-19 22:48:58 -0700

    Project1: Adding and populating table view with image names.  (p.118)

--- iOS/HackingWithSwift/StormViewer/StormViewer/ViewController.swift
+++ iOS/HackingWithSwift/StormViewer/StormViewer/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures: [String] = []     //could probably make this ! too
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        pictures = items.filter {$0.hasPrefix("nssl")}      //tutorial did loop with pictures.append()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
