0db0e6a25ad1a33744b1ef5d40fea1e6c3a46ee4
David Petrofsky <davidpet@ucla.edu>
2016-09-21 20:50:09 -0700

    Project2: Adding basic scene with 3 flags pre-populated.

---
+++ iOS/HackingWithSwift/Guess the Flag/Guess the Flag/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by David Petrofsky on 9/21/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland",
+                      "italy", "monaco", "nigeria", "poland", "russia", "spain",
+                      "uk", "us"]
+        askQuestion()
+    }
+
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
