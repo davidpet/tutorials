e3045cbbb45e8104f7d3128e867c831c874d4c4f
David Petrofsky <davidpet@ucla.edu>
2016-09-21 21:55:12 -0700

    Project2: Adding gameplay.

--- iOS/HackingWithSwift/Guess the Flag/Guess the Flag/ViewController.swift
+++ iOS/HackingWithSwift/Guess the Flag/Guess the Flag/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -51,7 +51,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }
+        else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
 }
 
