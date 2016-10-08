600f0d4be82381cd7a3bf3cdcc7343868e1d2c1d
David Petrofsky <davidpet@ucla.edu>
2016-10-08 01:51:40 -0700

    Project 4: Fixing spacing

--- iOS/HackingWithSwift/EasyBrowser/EasyBrowser/ViewController.swift
+++ iOS/HackingWithSwift/EasyBrowser/EasyBrowser/ViewController.swift
@@ -77,7 +77,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 if host.range(of: website) != nil {
                     decisionHandler(.allow)
                     return
-                } }
+                }
+            }
         }
         decisionHandler(.cancel)
     }
