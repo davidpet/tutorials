45915b2e31a18e7e5829f1b3ceb11194ec08ed92
David Petrofsky <davidpet@ucla.edu>
2016-10-07 21:50:49 -0700

    Milestone 2: Creating front-end flag list.

---
+++ iOS/HackingWithSwift/FlagViewer/FlagViewer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  FlagViewer
+//
+//  Created by David Petrofsky on 10/7/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var flags: [String] = ["estonia", "france", "germany", "ireland", "italy", "monaco",
+                           "nigeria", "poland", "russia", "spain", "uk", "us"]
+                            //had to hard-code because I decided to add flags to xcassets (mistake)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag")
+        cell?.imageView?.image = UIImage(named: flags[indexPath.row])
+        return cell!
+    }
+}
+
