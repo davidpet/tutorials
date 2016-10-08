ad442eea87f34e4c71221d640aef018d1a82000c
David Petrofsky <davidpet@ucla.edu>
2016-10-07 22:45:52 -0700

    Milestone 2: Adding detail page.

---
+++ iOS/HackingWithSwift/FlagViewer/FlagViewer/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  FlagViewer
+//
+//  Created by David Petrofsky on 10/7/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var flagImage: UIImageView!
+    var flagName: String!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = flagName
+        flagImage.image = UIImage(named: flagName)
+        flagImage.layer.borderWidth = 1
+        flagImage.layer.borderColor = UIColor.darkGray.cgColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS/HackingWithSwift/FlagViewer/FlagViewer/ViewController.swift
+++ iOS/HackingWithSwift/FlagViewer/FlagViewer/ViewController.swift
@@ -27,10 +27,16 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag")
-        cell?.imageView?.image = UIImage(named: flags[indexPath.row])
-        cell?.textLabel?.text = flags[indexPath.row]
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.imageView?.image = UIImage(named: flags[indexPath.row])
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as! DetailViewController
+        vc.flagName = flags[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
