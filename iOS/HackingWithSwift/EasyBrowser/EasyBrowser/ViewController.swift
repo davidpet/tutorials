30e6cb4dbc2eef0d62c9b23f6b7b075480c2d882
David Petrofsky <davidpet@ucla.edu>
2016-10-08 00:54:14 -0700

    Project4: Adding popup for navigating to different pages, plus title and Open button.

--- iOS/HackingWithSwift/EasyBrowser/EasyBrowser/ViewController.swift
+++ iOS/HackingWithSwift/EasyBrowser/EasyBrowser/ViewController.swift
@@ -21,11 +21,36 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://www.apple.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title:
+            "Open", style: .plain, target: self, action:
+            #selector(openTapped))
     }
-
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message:
+            nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com",
+                                   style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com",
+                                   style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
