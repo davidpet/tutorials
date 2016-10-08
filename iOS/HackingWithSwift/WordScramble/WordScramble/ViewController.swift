d288ed915870566c441d298e7dc81f4f8d72779e
David Petrofsky <davidpet@ucla.edu>
2016-10-08 12:42:00 -0700

    Project 5: Adding basic table view scene and navigation controller.

---
+++ iOS/HackingWithSwift/WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by David Petrofsky on 10/8/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
