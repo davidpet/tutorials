bc98ac97ecece9d36a987db0e110d195af641539
David Petrofsky <davidpet@ucla.edu>
2016-10-19 21:36:56 -0700

    Project 7: Adding navigation controller and tab bar controller with some very basic functionality.

--- iOS/HackingWithSwift/WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ iOS/HackingWithSwift/WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var petitions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
 }
 
