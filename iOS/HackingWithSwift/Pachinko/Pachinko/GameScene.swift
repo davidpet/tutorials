d6b7af6898c8c594e5975c4fcc01a9779e95838f
David Petrofsky <davidpet@ucla.edu>
2016-10-25 11:03:08 -0700

    Project 11: Setting background image.

--- iOS/HackingWithSwift/Pachinko/Pachinko/GameScene.swift
+++ iOS/HackingWithSwift/Pachinko/Pachinko/GameScene.swift
@@ -10,6 +10,12 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with
