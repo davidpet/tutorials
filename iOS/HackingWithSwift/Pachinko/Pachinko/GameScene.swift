bbce151a058f5781eb271743a6ad59d23cd1335b
David Petrofsky <davidpet@ucla.edu>
2016-10-25 11:42:08 -0700

    Project 11: Adding bouncer.

--- iOS/HackingWithSwift/Pachinko/Pachinko/GameScene.swift
+++ iOS/HackingWithSwift/Pachinko/Pachinko/GameScene.swift
@@ -18,6 +18,13 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = CGPoint(x: 512, y: 0)
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody!.isDynamic = false
+        
+        addChild(bouncer)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
