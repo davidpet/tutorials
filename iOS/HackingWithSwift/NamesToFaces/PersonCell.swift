264344205220b3f0b6a4423e0a947dc1b524dac5
David Petrofsky <davidpet@ucla.edu>
2016-10-24 22:44:00 -0700

    Project 10: Creating and connecting custom cell subclass.

---
+++ iOS/HackingWithSwift/NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by David Petrofsky on 10/24/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
