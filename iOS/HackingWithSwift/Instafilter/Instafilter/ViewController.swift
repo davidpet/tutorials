08f4aed199ba25e128c242605bdf0c3906ae8b64
David Petrofsky <davidpet@ucla.edu>
2016-10-26 01:05:53 -0700

    Project 13: Adding additional filter options for the Change Filter button

--- iOS/HackingWithSwift/Instafilter/Instafilter/ViewController.swift
+++ iOS/HackingWithSwift/Instafilter/Instafilter/ViewController.swift
@@ -51,14 +51,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func applyProcessing() {
-        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
-        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+        //set filter values that are supported by the filter (based on the 1 slider)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey)
+        { currentFilter.setValue(intensity.value, forKey:
+            kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey)
+        { currentFilter.setValue(intensity.value * 200, forKey:
+            kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey)
+        { currentFilter.setValue(intensity.value * 10, forKey:
+            kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey)
+        { currentFilter.setValue(CIVector(x: currentImage.size.width /
+            2, y: currentImage.size.height / 2), forKey:
+            kCIInputCenterKey) }
+        
+        //do the rendering
+        if let cgimg =
+            context.createCGImage(currentFilter.outputImage!, from:
+                currentFilter.outputImage!.extent) {
             let processedImage = UIImage(cgImage: cgimg)
-            imageView.image = processedImage
+            self.imageView.image = processedImage
         }
     }
     
+    func setFilter(action: UIAlertAction!) {
+        // make sure we have a valid image before continuing!
+        guard currentImage != nil else { return }
+        currentFilter = CIFilter(name: action.title!)
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
     @IBAction func changeFilter(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette",
+                                   style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
     }
     
     @IBAction func save(_ sender: UIButton) {
