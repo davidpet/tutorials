17d6a2825b78392bcd0bf057d20ff47b5f302370
David Petrofsky <davidpet@ucla.edu>
2016-10-26 00:28:31 -0700

    Project 13: Adding '+' button with image picker

--- iOS/HackingWithSwift/Instafilter/Instafilter/ViewController.swift
+++ iOS/HackingWithSwift/Instafilter/Instafilter/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
 
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self,
+                                                            action: #selector(importPicture))
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
+    func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
     @IBAction func changeFilter(_ sender: UIButton) {
     }
     
