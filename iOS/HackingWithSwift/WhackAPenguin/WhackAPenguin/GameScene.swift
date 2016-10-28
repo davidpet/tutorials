12ca13ee6239f3802c0a193b42a110952fea27bf
David Petrofsky <davidpet@ucla.edu>
2016-10-27 21:35:44 -0700

    Project 14: Adding background, score label, and image assets

--- iOS/HackingWithSwift/WhackAPenguin/WhackAPenguin/GameScene.swift
+++ iOS/HackingWithSwift/WhackAPenguin/WhackAPenguin/GameScene.swift
@@ -10,7 +10,26 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        score = 0
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
