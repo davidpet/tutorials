40c5142554e4b475ded79c33e4e59039696b4619
David Petrofsky <davidpet@ucla.edu>
2016-11-07 01:01:57 -0800

    Project 16: Adding code to print the javascript values passed in

--- iOS/HackingWithSwift/JavascriptExtension/Extension/ActionViewController.swift
+++ iOS/HackingWithSwift/JavascriptExtension/Extension/ActionViewController.swift
@@ -20,7 +20,9 @@ class ActionViewController: UIViewController {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [unowned self] (dict, error)
                     in
-                    // do stuff!
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey]                    as! NSDictionary
+                    print(javaScriptValues)
                 }
             }
         }
