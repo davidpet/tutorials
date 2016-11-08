3f8edb66c6ae38e1fa9a144727ec54487ccaca0f
David Petrofsky <davidpet@ucla.edu>
2016-11-07 23:28:29 -0800

    Project 17: Adding base SpriteKit project (after resetting to vanilla state)

---
+++ iOS/HackingWithSwift/SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by David Petrofsky on 11/7/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
