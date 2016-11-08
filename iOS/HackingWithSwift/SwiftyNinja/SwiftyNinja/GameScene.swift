43cb341c71e9d7a7816575e4d0f77d77a5476c5f
David Petrofsky <davidpet@ucla.edu>
2016-11-07 23:46:31 -0800

    Project 17: Adding lives and score to initial display

--- iOS/HackingWithSwift/SwiftyNinja/SwiftyNinja/GameScene.swift
+++ iOS/HackingWithSwift/SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -10,9 +10,14 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var gameScore: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
@@ -33,9 +38,22 @@ class GameScene: SKScene {
     }
     
     func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        gameScore.position = CGPoint(x: 8, y: 8)
     }
     
     func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)),
+                                          y: 720)
+            addChild(spriteNode)
+            livesImages.append(spriteNode)
+        }
     }
     
     func createSlices() {
