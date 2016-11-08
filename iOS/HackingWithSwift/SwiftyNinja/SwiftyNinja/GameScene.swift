0989400c475d09697a29988364d2a4a1014f3185
David Petrofsky <davidpet@ucla.edu>
2016-11-07 23:41:48 -0800

    Project 17: Adding background, physics, and basic initialization algorithm structure

--- iOS/HackingWithSwift/SwiftyNinja/SwiftyNinja/GameScene.swift
+++ iOS/HackingWithSwift/SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -15,8 +15,29 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    func createScore() {
+    }
+    
+    func createLives() {
+    }
+    
+    func createSlices() {
     }
 }
