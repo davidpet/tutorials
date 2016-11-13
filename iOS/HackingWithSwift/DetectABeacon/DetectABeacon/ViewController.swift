720f433fe95ba011835706fadd9217c640857391
David Petrofsky <davidpet@ucla.edu>
2016-11-12 20:52:57 -0800

    Project 22: Adding location authorization and detection of ranging capabilities

--- iOS/HackingWithSwift/DetectABeacon/DetectABeacon/ViewController.swift
+++ iOS/HackingWithSwift/DetectABeacon/DetectABeacon/ViewController.swift
@@ -7,20 +7,30 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var distanceReading: UILabel!
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = UIColor.gray
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    //handle iBeacon stuff here
+                }
+            }
+        }
     }
-
-
 }
-
