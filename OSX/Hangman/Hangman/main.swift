94976c3afba020abf36eddbb9617a93caeae7caa
David Petrofsky <davidpet@ucla.edu>
2016-11-12 19:16:32 -0800

    Pro Swift: Chapter 7: Adding base cmd-line project

---
+++ OSX/Hangman/Hangman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hangman
+//
+//  Created by David Petrofsky on 11/12/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
