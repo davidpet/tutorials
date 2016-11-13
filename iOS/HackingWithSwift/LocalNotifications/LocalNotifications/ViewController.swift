e1b2b59e55d7e595619df61a73525cffcbc7ab2c
David Petrofsky <davidpet@ucla.edu>
2016-11-12 18:37:58 -0800

    Project 21: Adding custom action as tutorial describes (which is not working for me)

--- iOS/HackingWithSwift/LocalNotifications/LocalNotifications/ViewController.swift
+++ iOS/HackingWithSwift/LocalNotifications/LocalNotifications/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,29 @@ class ViewController: UIViewController {
             #selector(scheduleLocal))
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void)
+    {
+        // pull out the buried userInfo dictionary
+        let userInfo = response.notification.request.content.userInfo
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier")
+            case "show":
+                // the user tapped our "show more info..." button
+                print("Show more information...")
+                break
+            default:
+                break
+            }
+        }
+        
+        completionHandler()
+    }
+    
     func registerLocal() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
@@ -34,6 +57,8 @@ class ViewController: UIViewController {
     }
     
     func scheduleLocal() {
+        registerCategories()
+        
         let center = UNUserNotificationCenter.current()
         
         let content = UNMutableNotificationContent()
@@ -50,6 +75,15 @@ class ViewController: UIViewController {
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        center.setNotificationCategories([category])
     }
 }
 
