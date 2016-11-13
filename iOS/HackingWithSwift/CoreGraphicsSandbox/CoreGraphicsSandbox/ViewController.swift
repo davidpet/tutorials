6653bfb677721f504262f4b28c53873f1e79df19
David Petrofsky <davidpet@ucla.edu>
2016-11-13 04:14:04 -0800

    Project 27: Adding rectangle and circle drawing

--- iOS/HackingWithSwift/CoreGraphicsSandbox/CoreGraphicsSandbox/ViewController.swift
+++ iOS/HackingWithSwift/CoreGraphicsSandbox/CoreGraphicsSandbox/ViewController.swift
@@ -22,11 +22,29 @@ class ViewController: UIViewController {
     func drawRectangle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
         let img = renderer.image { ctx in
-            // awesome drawing code
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
         }
         imageView.image = img
     }
 
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 5, y: 5, width: 502, height: 502)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        imageView.image = img
+    }
+    
     @IBAction func redrawTapped(_ sender: Any) {
         currentDrawType += 1
         if currentDrawType > 5 {
@@ -35,6 +53,8 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+        case 1:
+            drawCircle()
         default:
             break
         }
