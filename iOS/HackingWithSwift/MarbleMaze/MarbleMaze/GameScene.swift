8f2369ae1b2c4dfbc964cc898cc3912db8f2742b
David Petrofsky <davidpet@ucla.edu>
2016-11-13 01:50:28 -0800

    Project 26: Adding base project w/ assets

---
+++ iOS/HackingWithSwift/MarbleMaze/MarbleMaze/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  MarbleMaze
+//
+//  Created by David Petrofsky on 11/13/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
