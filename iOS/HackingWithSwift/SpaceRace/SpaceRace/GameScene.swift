ac0372eb8eef797cda845df28291e88f43ef5396
David Petrofsky <davidpet@ucla.edu>
2016-11-12 22:15:33 -0800

    Project 23: Adding destruction of ship on contact with enemies

--- iOS/HackingWithSwift/SpaceRace/SpaceRace/GameScene.swift
+++ iOS/HackingWithSwift/SpaceRace/SpaceRace/GameScene.swift
@@ -62,6 +62,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         player.position = location
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        let explosion = SKEmitterNode(fileNamed: "explosion")!
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        isGameOver = true
+        gameTimer.invalidate()
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         for node in children {
             if node.position.x < -300 {
