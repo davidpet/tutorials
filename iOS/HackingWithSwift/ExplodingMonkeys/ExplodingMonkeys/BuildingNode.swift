a22d8af579548d6692d297ce1aea95f5ebf45c27
David Petrofsky <davidpet@ucla.edu>
2016-11-13 23:17:24 -0800

    Project 29: Adding BuildingNode class

---
+++ iOS/HackingWithSwift/ExplodingMonkeys/ExplodingMonkeys/BuildingNode.swift
@@ -0,0 +1,14 @@
+//
+//  BuildingNode.swift
+//  ExplodingMonkeys
+//
+//  Created by David Petrofsky on 11/13/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import SpriteKit
+import UIKit
+
+class BuildingNode: SKSpriteNode {
+
+}
