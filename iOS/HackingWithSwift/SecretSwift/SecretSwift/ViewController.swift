db044e5b9492ec9070605d0078c6bd422354a15f
David Petrofsky <davidpet@ucla.edu>
2016-11-13 21:42:32 -0800

    Project 28: Adding keyboard handling for the text view

--- iOS/HackingWithSwift/SecretSwift/SecretSwift/ViewController.swift
+++ iOS/HackingWithSwift/SecretSwift/SecretSwift/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard),
+                                       name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard),
+                                       name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        if notification.name == NSNotification.Name.UIKeyboardWillHide {
+            secret.contentInset = UIEdgeInsets.zero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        secret.scrollIndicatorInsets = secret.contentInset
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
     }
 }
 
