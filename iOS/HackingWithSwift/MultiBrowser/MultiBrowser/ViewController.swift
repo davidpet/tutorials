34b31754c489b9ad8f4eedd5ca2692f22fdd70a1
David Petrofsky <davidpet@ucla.edu>
2016-11-18 00:40:39 -0800

    Project 31: Adding web view selection via tap

--- iOS/HackingWithSwift/MultiBrowser/MultiBrowser/ViewController.swift
+++ iOS/HackingWithSwift/MultiBrowser/MultiBrowser/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate, 
     @IBOutlet weak var addressBar: UITextField!
     @IBOutlet weak var stackView: UIStackView!
 
+    weak var activeWebView: UIWebView?      //weak because could go away if user deletes it
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +25,11 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate, 
         navigationItem.rightBarButtonItems = [delete, add]
     }
 
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
     func setDefaultTitle() {
         title = "Multibrowser"
     }
@@ -35,6 +42,26 @@ class ViewController: UIViewController, UITextFieldDelegate, UIWebViewDelegate, 
         
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.loadRequest(URLRequest(url: url))
+        
+        webView.layer.borderColor = UIColor.blue.cgColor
+        selectWebView(webView)
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+    }
+    
+    func selectWebView(_ webView: UIWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+    }
+    
+    func webViewTapped(_ recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? UIWebView {
+            selectWebView(selectedWebView)
+        }
     }
     
     func deleteWebView() {
