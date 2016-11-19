2c67e771b74d61328ccd7c353a7922bb9ea0eacc
David Petrofsky <davidpet@ucla.edu>
2016-11-18 18:11:31 -0800

    Project 33: Adding navigation bar and empty record whistle page

---
+++ iOS/HackingWithSwift/WhatsThatWhistle/WhatsThatWhistle/RecordWhistleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordWhistleViewController.swift
+//  WhatsThatWhistle
+//
+//  Created by David Petrofsky on 11/18/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+
+class RecordWhistleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
