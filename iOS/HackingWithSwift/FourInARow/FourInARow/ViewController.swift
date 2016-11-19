4958385ff4e182b84b8c9213c88f281bcf0c3c66
David Petrofsky <davidpet@ucla.edu>
2016-11-19 14:44:55 -0800

    Project 34: Setting up UI

--- iOS/HackingWithSwift/FourInARow/FourInARow/ViewController.swift
+++ iOS/HackingWithSwift/FourInARow/FourInARow/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var columnButtons: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func makeMove(_ sender: UIButton) {
+    }
 }
 
