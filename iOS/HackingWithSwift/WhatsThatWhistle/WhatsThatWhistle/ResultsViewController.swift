b31b89a89079dfd9eadfcb81c6472f583ab86bb8
David Petrofsky <davidpet@ucla.edu>
2016-11-18 22:19:18 -0800

    Project 33: Adding whistle detail table section code

---
+++ iOS/HackingWithSwift/WhatsThatWhistle/WhatsThatWhistle/ResultsViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ResultsViewController.swift
+//  WhatsThatWhistle
+//
+//  Created by David Petrofsky on 11/18/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CloudKit
+
+//Whistle detail page (from the table)
+class ResultsViewController: UITableViewController {
+    var whistle: Whistle!
+    var suggestions = [String]()
+    
+    var whistlePlayer: AVAudioPlayer!
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Suggested songs"
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return max(1, suggestions.count + 1)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.selectionStyle = .none
+        cell.textLabel?.numberOfLines = 0
+        
+        if indexPath.section == 0 {
+            // the user's comments about this whistle
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+            if whistle.comments.characters.count == 0 {
+                cell.textLabel?.text = "Comments: None"
+            } else {
+                cell.textLabel?.text = whistle.comments
+            }
+        } else {
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            if indexPath.row == suggestions.count {
+                // this is our extra row
+                cell.textLabel?.text = "Add suggestion"
+                cell.selectionStyle = .gray
+            } else {
+                cell.textLabel?.text = suggestions[indexPath.row]
+            }
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+}
