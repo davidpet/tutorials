2132d16b812796203392a0768e906cb3286b4edd
David Petrofsky <davidpet@ucla.edu>
2016-11-18 23:27:13 -0800

    Project 33: Adding ability to check/uncheck genres with persistence

--- iOS/HackingWithSwift/WhatsThatWhistle/WhatsThatWhistle/MyGenresViewController.swift
+++ iOS/HackingWithSwift/WhatsThatWhistle/WhatsThatWhistle/MyGenresViewController.swift
@@ -12,4 +12,63 @@ import CloudKit
 //page that lets you subscribe to push notifications for genres
 class MyGenresViewController: UITableViewController {
     var myGenres: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        if let savedGenres = defaults.object(forKey: "myGenres") as? [String] {
+            myGenres = savedGenres
+        } else {
+            myGenres = [String]()
+        }
+        
+        title = "Notify me about..."
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self,
+                                                            action: #selector(saveTapped))
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SelectGenreViewController.genres.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let selectedGenre = SelectGenreViewController.genres[indexPath.row]
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+                myGenres.append(selectedGenre)
+            } else {
+                cell.accessoryType = .none
+                if let index = myGenres.index(of: selectedGenre) {
+                    myGenres.remove(at: index)
+                }
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt
+        indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let genre = SelectGenreViewController.genres[indexPath.row]
+        cell.textLabel?.text = genre
+        if myGenres.contains(genre) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    func saveTapped() {
+        let defaults = UserDefaults.standard
+        defaults.set(myGenres, forKey: "myGenres")
+    }
 }
