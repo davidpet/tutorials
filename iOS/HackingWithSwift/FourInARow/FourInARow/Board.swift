2c9d14a7f8eeeadffd75fa22991aaf56c1f23a31
David Petrofsky <davidpet@ucla.edu>
2016-11-19 16:14:18 -0800

    Project 34: Adding win detection

--- iOS/HackingWithSwift/FourInARow/FourInARow/Board.swift
+++ iOS/HackingWithSwift/FourInARow/FourInARow/Board.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 enum ChipColor: Int {
     case none = 0
@@ -67,7 +68,40 @@ class Board: NSObject {
         return true
     }
     
-    func isWin(for player: Player) -> Bool {
+    func isWin(for player: GKGameModelPlayer) -> Bool {
+        let chip = (player as! Player).chip
+        //brute force: check each cell for a win in each direction
+        //even though only checks 4 directions, since checking every cell, it covers all the 8 neighbor lines
+        for row in 0 ..< Board.height {
+            for col in 0 ..< Board.width {
+                if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: 0) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 0, moveY: 1) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: 1) {
+                    return true
+                } else if squaresMatch(initialChip: chip, row: row, col: col, moveX: 1, moveY: -1) {
+                    return true
+                }
+            }
+        }
         return false
+    }
+    
+    //check for four from (row,col) to (row+movY*3, col+movX*3) of the proper color
+    //safe to call on edges
+    func squaresMatch(initialChip: ChipColor, row: Int, col: Int, moveX: Int, moveY: Int) -> Bool {
+        // bail out early if we can't win from here
+        if row + (moveY * 3) < 0 { return false }
+        if row + (moveY * 3) >= Board.height { return false }
+        if col + (moveX * 3) < 0 { return false }
+        if col + (moveX * 3) >= Board.width { return false }
+        // still here? Check every square
+        if chip(inColumn: col, row: row) != initialChip { return false }
+        if chip(inColumn: col + moveX, row: row + moveY) != initialChip { return false }
+        if chip(inColumn: col + (moveX * 2), row: row + (moveY * 2)) != initialChip { return false }
+        if chip(inColumn: col + (moveX * 3), row: row + (moveY * 3)) != initialChip { return false }
+        //if we make it here, this is a match
+        return true
     }
 }
