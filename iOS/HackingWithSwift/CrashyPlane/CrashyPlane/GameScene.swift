ce9792ac42dcdad452d47e1990557a8140429bd0
David Petrofsky <davidpet@ucla.edu>
2016-11-20 16:37:26 -0800

    Project 36: Adding sky

--- iOS/HackingWithSwift/CrashyPlane/CrashyPlane/GameScene.swift
+++ iOS/HackingWithSwift/CrashyPlane/CrashyPlane/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         createPlayer()
+        createSky()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,5 +35,27 @@ class GameScene: SKScene {
         let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame2], timePerFrame: 0.01)
         let runForever = SKAction.repeatForever(animation)
         player.run(runForever)
+    }
+    
+    func createSky() {
+        //create two sky portions that divide the height of the screen (sky and background ground)
+        let topSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.14, brightness: 0.97, alpha: 1),
+                                  size: CGSize(width: frame.width, height: frame.height * 0.67))
+        let bottomSky = SKSpriteNode(color: UIColor(hue: 0.55, saturation: 0.16, brightness: 0.96, alpha: 1),
+                                     size: CGSize(width: frame.width, height: frame.height * 0.33))
+        
+        //position the pieces of sky
+        topSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        bottomSky.anchorPoint = CGPoint(x: 0.5, y: 1)
+        topSky.position = CGPoint(x: frame.midX, y: frame.height)
+        bottomSky.position = CGPoint(x: frame.midX, y: bottomSky.frame.height)
+        
+        //sky is the bottommost thing
+        bottomSky.zPosition = -40
+        topSky.zPosition = -40
+        
+        //add to the scene
+        addChild(topSky)
+        addChild(bottomSky)
     }
 }
