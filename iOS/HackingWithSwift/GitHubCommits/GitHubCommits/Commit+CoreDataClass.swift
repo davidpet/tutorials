6f6754b76457f9551f6e6b217bfd1d158ee8c1ff
David Petrofsky <davidpet@ucla.edu>
2016-11-23 22:01:40 -0800

    Project 38: Adding class for data model

---
+++ iOS/HackingWithSwift/GitHubCommits/GitHubCommits/Commit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Commit+CoreDataClass.swift
+//  GitHubCommits
+//
+//  Created by David Petrofsky on 11/23/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
---
+++ iOS/HackingWithSwift/GitHubCommits/GitHubCommits/Commit+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Commit+CoreDataProperties.swift
+//  GitHubCommits
+//
+//  Created by David Petrofsky on 11/23/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Commit {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Commit> {
+        return NSFetchRequest<Commit>(entityName: "Commit");
+    }
+
+    @NSManaged public var date: Date
+    @NSManaged public var message: String
+    @NSManaged public var sha: String
+    @NSManaged public var url: String
+
+}
