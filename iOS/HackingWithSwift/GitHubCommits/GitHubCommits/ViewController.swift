491c4da7b3a506a08dc7fdfb5284507403e231dc
David Petrofsky <davidpet@ucla.edu>
2016-11-23 22:26:46 -0800

    Project 38: Saving Github commits into sqlite

--- iOS/HackingWithSwift/GitHubCommits/GitHubCommits/ViewController.swift
+++ iOS/HackingWithSwift/GitHubCommits/GitHubCommits/ViewController.swift
@@ -21,6 +21,33 @@ class ViewController: UITableViewController {
                 print("Unresolved error \(error)")
             }
         }
+        
+        performSelector(inBackground: #selector(fetchCommits), with: nil)
+    }
+    
+    func fetchCommits() {
+        if let data = try? Data(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!){
+            let jsonCommits = JSON(data: data)
+            let jsonCommitArray = jsonCommits.arrayValue
+            print("Received \(jsonCommitArray.count) new commits.")
+            DispatchQueue.main.async { [unowned self] in
+                for jsonCommit in jsonCommitArray {
+                    // the following three lines are new
+                    let commit = Commit(context: self.container.viewContext)
+                    self.configure(commit: commit, usingJSON: jsonCommit)
+                }
+                self.saveContext()
+            }
+        }
+    }
+
+    func configure(commit: Commit, usingJSON json: JSON) {
+        commit.sha = json["sha"].stringValue
+        commit.message = json["commit"]["message"].stringValue
+        commit.url = json["html_url"].stringValue
+        
+        let formatter = ISO8601DateFormatter()
+        commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
     }
     
     func saveContext() {
