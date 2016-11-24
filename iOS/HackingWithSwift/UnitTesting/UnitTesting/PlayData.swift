1e69c318e042c04fa14605031909a0d8d9d1689c
David Petrofsky <davidpet@ucla.edu>
2016-11-24 02:06:50 -0800

    Project 39: Adding basic app data

---
+++ iOS/HackingWithSwift/UnitTesting/UnitTesting/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  UnitTesting
+//
+//  Created by David Petrofsky on 11/24/16.
+//  Copyright © 2016 David Petrofsky. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allWords = [String]()
+}
