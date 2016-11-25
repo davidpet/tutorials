ff99a2a3045e74ba4fab20505bbd2227da5e2a5b
David Petrofsky <davidpet@ucla.edu>
2016-11-24 23:29:42 -0800

    Project 39: De-duplicating words

--- iOS/HackingWithSwift/UnitTesting/UnitTesting/PlayData.swift
+++ iOS/HackingWithSwift/UnitTesting/UnitTesting/PlayData.swift
@@ -10,12 +10,23 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
+    var uniqueWords = [String:Int]()
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
             if let plays = try? String(contentsOfFile: path) {
                 allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
                 allWords = allWords.filter { $0 != "" }
+                
+                for word in allWords {
+                    if uniqueWords[word] == nil {
+                        uniqueWords[word] = 1
+                    } else {
+                        uniqueWords[word]! += 1
+                    }
+                }
+                
+                allWords = Array(uniqueWords.keys)
             }
         }
     }
--- iOS/HackingWithSwift/UnitTesting/UnitTestingTests/UnitTestingTests.swift
+++ iOS/HackingWithSwift/UnitTesting/UnitTestingTests/UnitTestingTests.swift
@@ -23,6 +23,6 @@ class UnitTestingTests: XCTestCase {
     
     func testAllWordsLoaded() {
         let playData = PlayData()
-        XCTAssertEqual(playData.allWords.count, 384001, "allWords was not 384001")
+        XCTAssertEqual(playData.allWords.count, 18440, "allWords was not 18440")
     }
 }
