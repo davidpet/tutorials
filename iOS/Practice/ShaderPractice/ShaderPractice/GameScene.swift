f8ccec1a36f89d51f66c79f8b01c2aa05882133b
David Petrofsky <davidpet@ucla.edu>
2016-11-26 04:16:53 -0800

    Adding first shader and rotation on tap functionality

--- iOS/Practice/ShaderPractice/ShaderPractice/GameScene.swift
+++ iOS/Practice/ShaderPractice/ShaderPractice/GameScene.swift
@@ -10,12 +10,27 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var shield: SKSpriteNode!
+    var shaders = [SKShader?]()
+    var currentShader = 0
+    
     override func didMove(to view: SKView) {
         let shield = SKSpriteNode(imageNamed: "Shield")
         shield.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
         addChild(shield)
+        self.shield = shield
+        
+        shaders.append(nil)
+        shaders.append(SKShader(fileNamed: "shader1"))
+        
+        shield.shader = shaders[currentShader]
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        currentShader += 1
+        if currentShader >= shaders.count {
+            currentShader = 0
+        }
+        shield.shader = shaders[currentShader]
     }
 }
