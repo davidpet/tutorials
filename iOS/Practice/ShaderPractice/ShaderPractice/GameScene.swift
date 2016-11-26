d141f2ac692f5da32147f3eb4dabb516c806e65f
David Petrofsky <davidpet@ucla.edu>
2016-11-26 03:40:07 -0800

    Adding shield graphic

--- iOS/Practice/ShaderPractice/ShaderPractice/GameScene.swift
+++ iOS/Practice/ShaderPractice/ShaderPractice/GameScene.swift
@@ -11,6 +11,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        let shield = SKSpriteNode(imageNamed: "Shield")
+        shield.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        addChild(shield)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
