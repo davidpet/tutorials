4aef66a9139f6e4b6fc58b6ab0978ec591c0b03b
David Petrofsky <davidpet@ucla.edu>
2016-11-26 00:32:46 -0800

    Adding notes about MARK

--- iOS/Playgrounds/SwiftComments.playground/Contents.swift
+++ iOS/Playgrounds/SwiftComments.playground/Contents.swift
@@ -1,3 +1,7 @@
 // reagular comment
 /* block comment */
-   
+
+//MARK: NSObject
+
+//MARK: MyOtherCategory
+//these comments create logical sections in the code which show up when you pull down the list of members to jump to
