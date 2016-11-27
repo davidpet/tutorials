47ecb0ceed443c50d2e5add4ab7655e563cc9fcf
David Petrofsky <davidpet@ucla.edu>
2016-11-27 00:41:43 -0800

    Refactoring and cleaning up ShieldSprite class

--- iOS/Practice/ShaderPractice/ShaderPractice/ShieldSprite.swift
+++ iOS/Practice/ShaderPractice/ShaderPractice/ShieldSprite.swift
@@ -10,43 +10,49 @@ import UIKit
 import SpriteKit
 
 class ShieldSprite: SKNode {
+    //MARK: Static Variables
     public static let nodeName = "Shield"
     
     private static let texture = SKTexture(imageNamed: "Shield")
     private static let maskTexture = SKTexture(imageNamed: "shieldmask")
     
     private static let shaders: [SKShader?] = makeShaders()
+    private static let uniforms = [
+        SKUniform(name: "u_gradient", texture: SKTexture(imageNamed: "shieldmask")),
+        SKUniform(name: "u_health", float: 0.75),
+        SKUniform(name: "u_size", vectorFloat2: vector_float2([Float(texture.size().width /** UIScreen.main.scale*/),
+                                                               Float(texture.size().height /** UIScreen.main.scale*/)]))
+    ]
+    private static let lastShader = 6
     
+    //MARK Private Variables
     private var currentShader = 0
     private var shield: SKSpriteNode!
     
+    //MARK: Static Methods
     private static func makeShaders() -> [SKShader?] {
         var shaders = [SKShader?]()
-        
         shaders.append(nil)
-        shaders.append(SKShader(fileNamed: "shader1"))
-        shaders.append(SKShader(fileNamed: "shader2"))
-        let pixelSize = vector_float2([Float(texture.size().width /** UIScreen.main.scale*/),
-                                       Float(texture.size().height /** UIScreen.main.scale*/)])
-        let uniforms = [
-            SKUniform(name: "u_gradient", texture: SKTexture(imageNamed: "shieldmask")),
-            SKUniform(name: "u_health", float: 0.75),
-            SKUniform(name: "u_size", vectorFloat2: pixelSize)
-        ]
-        let shader3 = SKShader(fileNamed: "shader3")
-        shader3.uniforms = uniforms
-        shaders.append(shader3)
-        let shader4 = SKShader(fileNamed: "shader4")
-        shader4.uniforms = uniforms
-        shaders.append(shader4)
-        let shader5 = SKShader(fileNamed: "shader5")
-        shader5.uniforms = uniforms
-        shaders.append(shader5)
-        shaders.append(SKShader(fileNamed: "shader6"))
+        
+        var shaderHasUniforms = [Bool]()
+        (0...lastShader).forEach { _ in shaderHasUniforms.append(false) }
+        (3...5).forEach { shaderHasUniforms[$0] = true }
+        
+        (1...lastShader).forEach { shaders.append(makeShader($0, withUniforms: shaderHasUniforms[$0])) }
         
         return shaders
     }
     
+    private static func makeShader(_ num: Int, withUniforms: Bool) -> SKShader {
+        let shader = SKShader(fileNamed: "shader\(num)")
+        if withUniforms {
+            shader.uniforms = ShieldSprite.uniforms
+        }
+        
+        return shader
+    }
+    
+    //MARK: Constructors
     public init(at position: CGPoint) {
         super.init()
         self.position = position
@@ -62,6 +68,7 @@ class ShieldSprite: SKNode {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: Public Methods
     public func advanceShader() {
         currentShader = (currentShader + 1) % ShieldSprite.shaders.count
         
