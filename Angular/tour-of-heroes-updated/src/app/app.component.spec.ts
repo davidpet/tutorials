93a6c27d79589e77bc9ea6c653af266812e181c7
David Petrofsky <davidpet@ucla.edu>
2023-05-24 01:50:21 -0700

    Step 1A: Adding heroes component.

--- Angular/tour-of-heroes-updated/src/app/app.component.spec.ts
+++ Angular/tour-of-heroes-updated/src/app/app.component.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent, HeroesComponent]
   }));
 
   it('should create the app', () => {
@@ -23,5 +24,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Tour of Heroes');
+  });
+
+  it('should render app heroes component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const childComponent = fixture.debugElement.nativeElement.querySelector('app-heroes');
+    expect(childComponent).toBeTruthy();
   });
 });
