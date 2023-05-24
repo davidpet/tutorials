ef4339bc3a5601c8a31856209ed7f6269b88fb2d
David Petrofsky <davidpet@ucla.edu>
2023-05-24 16:53:15 -0700

    Step 4D: Cleaning up subscription.

--- Angular/tour-of-heroes-updated/src/app/heroes/heroes.component.ts
+++ Angular/tour-of-heroes-updated/src/app/heroes/heroes.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
@@ -23,10 +26,18 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.heroService
+      .getHeroes()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((heroes) => (this.heroes = heroes));
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
