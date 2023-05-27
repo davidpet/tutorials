073bd629dedb4c03f07974bd24e524e9a85a16ce
David Petrofsky <davidpet@ucla.edu>
2023-05-27 10:36:05 -0700

    Setting form control value from TS code.

--- Angular/reactive-form/src/app/reactive-form/reactive-form.component.ts
+++ Angular/reactive-form/src/app/reactive-form/reactive-form.component.ts
@@ -9,4 +9,12 @@ import { FormControl } from '@angular/forms';
 export class ReactiveFormComponent {
   // Form is driven from here instead of the template.
   readonly textControl = new FormControl('');
+
+  setValue() {
+    // Reflected in text box right away because change detection will run
+    // after the click handler returns.
+    // In a template-driven form, you would just set the two-way bound
+    // value here.
+    this.textControl.setValue('42');
+  }
 }
