b5f40cd630426ff667d2ee06a127c8b5454af885
David Petrofsky <davidpet@ucla.edu>
2023-05-27 10:42:27 -0700

    Listening to observable of form control.

--- Angular/reactive-form/src/app/reactive-form/reactive-form.component.ts
+++ Angular/reactive-form/src/app/reactive-form/reactive-form.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss'],
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+  textChanges$!: Observable<string | null>;
+
   // Form is driven from here instead of the template.
   readonly textControl = new FormControl('');
+
+  ngOnInit() {
+    this.textChanges$ = this.textControl.valueChanges;
+  }
 
   setValue() {
     // Reflected in text box right away because change detection will run
     // after the click handler returns.
     // In a template-driven form, you would just set the two-way bound
     // value here.
+    // It also emits to the observable.
+    // NOTE: setValue() is called both by this code and by the directive
+    // when the user changes the input text manually, triggering
+    // the observable to emit.
     this.textControl.setValue('42');
   }
 }
