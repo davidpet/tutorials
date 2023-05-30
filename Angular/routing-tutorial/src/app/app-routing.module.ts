db5fbbf5ae1cc677651101d812b2db0a0ffe820e
David Petrofsky <davidpet@ucla.edu>
2023-05-30 10:03:08 -0700

    Adding nested (child) routes.

--- Angular/routing-tutorial/src/app/app-routing.module.ts
+++ Angular/routing-tutorial/src/app/app-routing.module.ts
@@ -3,11 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
 const routes: Routes = [
   // Order is more specific to less specific because first
   // match wins.
-  { path: 'first-component', component: FirstComponent },
+  {
+    path: 'first-component',
+    component: FirstComponent,
+    // Within FirstComponent, you can have another unnamed
+    // outlet and router links that pretend it's the root.
+    children: [
+      {
+        path: 'child-a',
+        component: ChildAComponent,
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent,
+      },
+    ],
+  },
   { path: 'second-component', component: SecondComponent },
 
   // Default empty path to first component (shows in address bar).
@@ -18,7 +35,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [FirstComponent, SecondComponent, PageNotFoundComponent],
+  declarations: [
+    FirstComponent,
+    SecondComponent,
+    PageNotFoundComponent,
+    ChildAComponent,
+    ChildBComponent,
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
