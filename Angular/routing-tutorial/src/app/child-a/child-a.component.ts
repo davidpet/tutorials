cb846098715d3e3a96a8b7a8afd8812ce1fb5993
David Petrofsky <davidpet@ucla.edu>
2023-05-30 14:36:09 -0700

    Adding matrix url params and router.navigate relative.

--- Angular/routing-tutorial/src/app/child-a/child-a.component.ts
+++ Angular/routing-tutorial/src/app/child-a/child-a.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-child-a',
   templateUrl: './child-a.component.html',
-  styleUrls: ['./child-a.component.scss']
+  styleUrls: ['./child-a.component.scss'],
 })
-export class ChildAComponent {
+export class ChildAComponent implements OnInit {
+  id = -1000;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // We can just take the params we're interested in and ignore
+    // the rest.  We could check for existence, use observable
+    // instead, etc. as needed.
+    // NOTE: This is the same way you get params that are part
+    // of the URL, but not the same way you get query params.
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+  }
 }
--- Angular/routing-tutorial/src/app/first/first.component.ts
+++ Angular/routing-tutorial/src/app/first/first.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 import { Observable, map } from 'rxjs';
 
 @Component({
@@ -13,7 +13,7 @@ export class FirstComponent implements OnInit {
 
   id$?: Observable<string>;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     if (this.id === undefined) {
@@ -32,5 +32,41 @@ export class FirstComponent implements OnInit {
         map((paramMap) => paramMap.get('id') ?? '')
       );
     }
+  }
+
+  navigateToA() {
+    // takes an array of URL segments
+    // The routerLink version automatically made it relative, but
+    // here you have to do a little extra to make that happen.
+
+    // Note that you can pass in whatever you want as the params
+    // and none of them had to be specified in the router.  This
+    // will use matrix url syntax instead of url path syntax for
+    // the params. This is NOT INTRECHANGEABLE WITH QUERYPARAMS.
+    // It does allow for optional values that you may or may not
+    // specify.
+    // http://localhost:55485/first-component/42/child-a;id=1000;foo=whocares
+    this.router.navigate(['child-a', { id: 1000, foo: 'whocares' }], {
+      relativeTo: this.route,
+    });
+  }
+
+  // This is needed for the styling and aria stuff since they
+  // don't support relativeTo like above.
+  getchildAUrl() {
+    const url = [...this.route.snapshot.url];
+    url.push(new UrlSegment('child-a', {}));
+
+    return url.map((seg) => seg.path).join('/');
+  }
+
+  // for styling
+  isActive(): boolean {
+    return this.router.isActive(this.getchildAUrl(), false);
+  }
+
+  //for aria
+  isCurrent(): string {
+    return this.router.isActive(this.getchildAUrl(), false) ? 'page' : '';
   }
 }
