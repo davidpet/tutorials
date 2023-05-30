0b6c4cbb475e65553a2c665c717bcde1ca061ba1
David Petrofsky <davidpet@ucla.edu>
2023-05-30 09:50:59 -0700

    Adding wildcard route with 404 component.

--- Angular/routing-tutorial/src/app/app-routing.module.ts
+++ Angular/routing-tutorial/src/app/app-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   // Order is more specific to less specific because first
   // match wins.
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
+
+  // Any path that gets to this point is a 404.
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  declarations: [FirstComponent, SecondComponent],
+  declarations: [FirstComponent, SecondComponent, PageNotFoundComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
