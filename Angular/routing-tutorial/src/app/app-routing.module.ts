2788afe4d7c42d5922a44dfde025d44b784681ea
David Petrofsky <davidpet@ucla.edu>
2023-05-30 09:30:05 -0700

    Adding 2 routed components w/ links.

--- Angular/routing-tutorial/src/app/app-routing.module.ts
+++ Angular/routing-tutorial/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Order is more specific to less specific because first
+  // match wins.
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
+  declarations: [FirstComponent, SecondComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
