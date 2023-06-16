c5b0729525ac1c2ab8527e5c5167f658bd515883
David Petrofsky <davidpet@ucla.edu>
2023-06-15 21:40:57 -0700

    Using GUAVA via Maven.

--- Bazel/examples/java-tutorial/src/main/java/com/example/Greeting.java
+++ Bazel/examples/java-tutorial/src/main/java/com/example/Greeting.java
@@ -1,7 +1,16 @@
 package com.example;
 
+// This is available because of the BUILD dep we set up.
+// It is part of GUAVA.
+import com.google.common.collect.ImmutableList;
+
 public class Greeting {
     public static void sayHi() {
-        System.out.println("Hi!");
+        ImmutableList<String> list = ImmutableList.<String>builder()
+            .add("Hello")
+            .add("World")
+            .build();
+
+        System.out.println(list);
     }
 }
