b3e654d3baaff028039f8582e5252dd441e84c93
David Petrofsky <davidpet@ucla.edu>
2023-07-17 15:50:05 -0700

    Using my-lib in my-app.
    Changes to my-lib aren't seen unless you:
    `ng build my-lib`
    It doesn't matter if you stop and restart the ng serve.

--- Angular/bazel-workspace/apps/my-app/src/app/app.module.ts
+++ Angular/bazel-workspace/apps/my-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyLibModule } from 'my-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
