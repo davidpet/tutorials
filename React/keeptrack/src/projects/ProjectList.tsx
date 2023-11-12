e903e92a3c6635cb4f4f37ef5a092f5792c786c6
David Petrofsky <davidpet@ucla.edu>
2023-11-12 12:43:52 -0800

    Lab 11: Parent to Child Communication

--- React/keeptrack/src/projects/ProjectList.tsx
+++ React/keeptrack/src/projects/ProjectList.tsx
@@ -7,11 +7,13 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+    const handleEdit = (project: Project) => { console.log(project);};
+
     return (
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
-                    <ProjectCard project={project}></ProjectCard>
+                    <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
                     <ProjectForm />
                 </div>
             ))}
