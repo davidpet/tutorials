ffc6482f01389bf0d630b4722030a4c3b66b0c6b
David Petrofsky <davidpet@ucla.edu>
2023-11-12 14:00:56 -0800

    Lab 13: Cancel button

--- React/keeptrack/src/projects/ProjectList.tsx
+++ React/keeptrack/src/projects/ProjectList.tsx
@@ -10,13 +10,14 @@ interface ProjectListProps {
 function ProjectList({ projects }: ProjectListProps) {
     const [projectBeingEdited, setProjectBeingEdited] = useState({});
     const handleEdit = (project: Project) => { setProjectBeingEdited(project);};
+    const cancelEditing = () => { setProjectBeingEdited({});};
 
     return (
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
                               {project === projectBeingEdited ? (
-                    <ProjectForm />
+                    <ProjectForm onCancel={cancelEditing}/>
                     ) : (
                     <ProjectCard project={project} onEdit={handleEdit} />
                                  )}
