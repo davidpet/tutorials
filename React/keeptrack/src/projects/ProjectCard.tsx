af6a341f5defb4d0c0a61a2dc30b394983847bd1
David Petrofsky <davidpet@ucla.edu>
2023-11-12 18:57:28 -0800

    Lab 21: Route Parameters

--- React/keeptrack/src/projects/ProjectCard.tsx
+++ React/keeptrack/src/projects/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import { Project } from './Project';
+import { Link } from 'react-router-dom';
 
 function formatDescription(description: string): string {
   return description.substring(0, 60) + '...';
@@ -19,6 +20,7 @@ function ProjectCard(props: ProjectCardProps) {
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
       <section className="section dark">
+       <Link to={'/projects/' + project.id}>
         <h5 className="strong">
           <strong>{project.name}</strong>
         </h5>
@@ -29,6 +31,7 @@ function ProjectCard(props: ProjectCardProps) {
             <span className="icon-edit "></span>
             Edit
         </button>
+       </Link>
       </section>
     </div>
   );
