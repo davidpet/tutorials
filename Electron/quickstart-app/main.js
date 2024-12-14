8b5e564688125bede18cd0d8394460fab258c2e8
David Petrofsky <davidpet@ucla.edu>
2024-12-13 17:29:10 -0800

    Adding preload script to populate versions.

--- Electron/quickstart-app/main.js
+++ Electron/quickstart-app/main.js
@@ -1,9 +1,17 @@
 import { app, BrowserWindow } from 'electron'; // I changed this, as tutorial does 'require'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'), // absolute path required
+      },
     })
   
     win.loadFile('index.html')
